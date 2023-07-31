9ba74f6f44ec566128484f6ba1de3b3a715ec37f
Jin Peiyuan <JinPeiyuan@JohnDeere.com>
2023-07-30 21:35:44 -0500

    db is working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,8 @@ function App() {
         return (
           poke
         )
-      })}
+      })} 
+
     </div>
   );
 }
--- pages/api/bj/[numPokemon].ts
+++ pages/api/bj/[numPokemon].ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type {NextApiRequest, NextApiResponse} from 'next';
+import { Sequelize, DataTypes } from 'sequelize';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log('request query', req.query)
@@ -17,28 +18,53 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     //         id : getContent.id
     //     })
     // }
-    const { Pool } = require('pg');
-    const test : string[] = [];
-    const pool = new Pool({
-        user: 'barneyjin',
-        host: 'rdsdb-test.c3vg2w4k8zot.us-east-2.rds.amazonaws.com',
-        database: 'barneytest',
-        password: 'Jinpy20010817',
-        port: '5432', // PostgreSQL default port
-    });
-    pool.query('SELECT * FROM your_table', (err: any, results: any) => {
-        if (err) {
-          console.error('Error executing query:', err);
-        } else {
-          console.log('Query results:', results.rows);
-          test.push(results.rows.name);
-        }
-    });
+    // const { Pool } = require('pg');
+    // const test : string[] = [];
+    // const pool = new Pool({
+    //     user: 'barneyjin',
+    //     host: 'rdsdb-test.c3vg2w4k8zot.us-east-2.rds.amazonaws.com',
+    //     database: 'barneytest',
+    //     password: 'Jinpy20010817',
+    //     port: '5432', // PostgreSQL default port
+    // });
+    // pool.query('SELECT * FROM your_table', (err: any, results: any) => {
+    //     if (err) {
+    //       console.error('Error executing query:', err);
+    //     } else {
+    //       console.log('Query results:', results.rows);
+    //       test.push(results.rows.name);
+    //     }
+    // });
 
+    const sequelize = new Sequelize({
+      database: "barneytest",
+      username: "barneyjin",
+      password: "Jinpy20010817",
+      host: "rdsdb-test.c3vg2w4k8zot.us-east-2.rds.amazonaws.com",
+      port: 5432,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true, // This will help you. But you will see nwe error
+          rejectUnauthorized: false // This line will fix new error
+        }
+      },
+    });
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+    const { QueryTypes } = require('sequelize');
+    const names = await sequelize.query(`select "name" from "Pokemons"`, { type: QueryTypes.SELECT });
+    
+    const pokenames = names.map(item => item.name);
+    console.log(pokenames)
     return res.status(200).json({
         statusCode: 200,
         message: 'Fetched Pokemons',
-        data: test
+        data: pokenames
         //data: pokemonArr
     });
 }
